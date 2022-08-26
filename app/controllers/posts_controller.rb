7357824a00fefa7f15fe89ec9d36f9e3970cfc0a
tonesou <tonegawa0525@gmail.com>
2022-08-26 06:28:42 +0000

    post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,30 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:shop, :explain)
   end
 end
