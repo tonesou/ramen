3eb31897aaee9d73b263448aaebe58e4a3c2713a
tonesou <tonegawa0525@gmail.com>
2022-08-29 06:13:52 +0000

    active record

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :images
+  
+  def get_image(width, height)
+    unless images.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
